11a3a0f61f092b7c895a01e2a37e291693c14f96
Eric Bryan <92419841+erbiker@users.noreply.github.com>
2021-12-16 18:56:03 +0000

    fully functional app. Added gif submission and user identification

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,40 @@
 import React, { useEffect, useState } from 'react';
+
+import { Connection, PublicKey, clusterApiUrl } from '@solana/web3.js';
+import { Program, Provider, web3 } from '@project-serum/anchor';
+
 import twitterLogo from './assets/twitter-logo.svg';
 import './App.css';
 
+import kp from './keypair.json'
+import idl from './idl.json'
+
+import { Buffer } from 'buffer';
+window.Buffer = Buffer;
+
 // Constants
+// SystemProgram is a reference to the Solana runtime!
+const { SystemProgram, Keypair } = web3;
+// Create a keypair for the account that will hold the GIF data.
+const arr = Object.values(kp._keypair.secretKey)
+const secret = new Uint8Array(arr)
+const baseAccount = web3.Keypair.fromSecretKey(secret)
+// Get our program's id from the IDL file.
+const programID = new PublicKey(idl.metadata.address);
+// Set our network to devnet.
+const network = clusterApiUrl('devnet');
+// Controls how we want to acknowledge when a transaction is "done".
+const opts = {
+  preflightCommitment: "processed"
+}
+
+const TEST_GIFS = [
+	'https://media3.giphy.com/media/7EOu3A9mywY2QkWlIn/giphy.gif?cid=ecf05e47yejicgsp32nsgwtuvnsbvxkx8jmkr2vcwpwgisfu&rid=giphy.gif&ct=g',
+	'https://media0.giphy.com/media/jbK2wDBCCYbXRhGtg3/giphy.gif?cid=ecf05e47kqa7727oqzip1cyk1thmkv5kckom6iwjr1czdaor&rid=giphy.gif&ct=g',
+	'https://media4.giphy.com/media/mCOSFkTdqOz8JcT0DK/giphy.gif?cid=ecf05e47b4l0a3ope05zdz7hrgzw2mpfd9kzeht0y8dkr14x&rid=giphy.gif&ct=g',
+	'https://media2.giphy.com/media/JQ4mudfr0yLS6R74dO/giphy.gif?cid=ecf05e47gx9qnu5f2o2lxaeeyx51y78kvdybpkl5xvhf1u0g&rid=giphy.gif&ct=g',
+]
+
 const TWITTER_HANDLE = 'erbiker_ebc';
 const TWITTER_LINK = `https://twitter.com/${TWITTER_HANDLE}`;
 
@@ -12,14 +44,6 @@ const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
   const [inputValue, setInputValue] = useState('');
   const [gifList, setGifList] = useState([]);
-
-  const TEST_GIFS = [
-	'https://media3.giphy.com/media/7EOu3A9mywY2QkWlIn/giphy.gif?cid=ecf05e47yejicgsp32nsgwtuvnsbvxkx8jmkr2vcwpwgisfu&rid=giphy.gif&ct=g',
-	'https://media0.giphy.com/media/jbK2wDBCCYbXRhGtg3/giphy.gif?cid=ecf05e47kqa7727oqzip1cyk1thmkv5kckom6iwjr1czdaor&rid=giphy.gif&ct=g',
-	'https://media4.giphy.com/media/mCOSFkTdqOz8JcT0DK/giphy.gif?cid=ecf05e47b4l0a3ope05zdz7hrgzw2mpfd9kzeht0y8dkr14x&rid=giphy.gif&ct=g',
-	'https://media2.giphy.com/media/JQ4mudfr0yLS6R74dO/giphy.gif?cid=ecf05e47gx9qnu5f2o2lxaeeyx51y78kvdybpkl5xvhf1u0g&rid=giphy.gif&ct=g',
-]
-
 
   /*
    * This function holds the logic for deciding if a Phantom Wallet is
@@ -71,12 +95,27 @@ const App = () => {
   };
 
   const sendGif = async () => {
-    if (inputValue.length > 0) {
-      console.log('Gif link:', inputValue);
-      setGifList([...gifList, inputValue]);
-      setInputValue('');
-    } else {
-      console.log('Empty input. Try again.');
+    if (inputValue.length === 0) {
+      console.log("No gif link given!")
+      return
+    }
+    setInputValue('');
+    console.log('Gif link:', inputValue);
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+
+      await program.rpc.addGif(inputValue, {
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+        },
+      });
+      console.log("GIF successfully sent to program", inputValue)
+
+      await getGifList();
+    } catch (error) {
+      console.log("Error sending GIF:", error)
     }
   };
 
@@ -84,6 +123,50 @@ const App = () => {
     const { value } = event.target;
     setInputValue(value);
   };
+
+  const getProvider = () => {
+    const connection = new Connection(network, opts.preflightCommitment);
+    const provider = new Provider(
+      connection, window.solana, opts.preflightCommitment,
+    );
+    return provider;
+  }
+
+  const getGifList = async() => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+      
+      console.log("Got the account", account)
+      setGifList(account.gifList)
+
+    } catch (error) {
+      console.log("Error in getGifList: ", error)
+      setGifList(null);
+    }
+  }
+
+  const createGifAccount = async () => {
+    try {
+      const provider = getProvider();
+      const program = new Program(idl, programID, provider);
+      console.log("ping")
+      await program.rpc.startStuffOff({
+        accounts: {
+          baseAccount: baseAccount.publicKey,
+          user: provider.wallet.publicKey,
+          systemProgram: SystemProgram.programId,
+        },
+        signers: [baseAccount]
+      });
+      console.log("Created a new BaseAccount w/ address:", baseAccount.publicKey.toString())
+      await getGifList();
+
+    } catch(error) {
+      console.log("Error creating BaseAccount account:", error)
+    }
+  }
 
   /*
    * We want to render this UI when the user hasn't connected
@@ -98,32 +181,51 @@ const App = () => {
     </button>
   );
 
-  const renderConnectedContainer = () => (
-    <div className="connected-container">
-      {/* Go ahead and add this input and button to start */}
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          sendGif();
-        }}
-      >
-        <input
-          type="text"
-          placeholder="Enter gif link!"
-          value={inputValue}
-          onChange={onInputChange}
-        />
-        <button type="submit" className="cta-button submit-gif-button">Submit</button>
-      </form>
-      <div className="gif-grid">
-        {gifList.map(gif => (
-          <div className="gif-item" key={gif}>
-            <img src={gif} alt={gif} />
+  const renderConnectedContainer = () => {
+    // If we hit this, it means the program account hasn't been initialized.
+    if (gifList === null) {
+      return (
+        <div className="connected-container">
+          <p className="username"> User: {walletAddress}
+          </p>
+          <button className="cta-button submit-gif-button" onClick={createGifAccount}>
+            Do One-Time Initialization For GIF Program Account
+          </button>
+        </div>
+      );
+    } else {
+      return(
+        // Otherwise, we're good! Account exists. User can submit GIFs.
+        <div className="connected-container">
+          <p className="username"> User: {walletAddress}
+          </p>
+          {/* Go ahead and add this input and button to start */}
+          <form
+            onSubmit={(event) => {
+              event.preventDefault();
+              sendGif();
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Enter gif link!"
+              value={inputValue}
+              onChange={onInputChange}
+            />
+            <button type="submit" className="cta-button submit-gif-button">Submit</button>
+          </form>
+          <div className="gif-grid">
+            {/* We use index as the key instead, also, the src is now item.gifLink */}
+            {gifList.map((item, index) => (
+              <div className="gif-item" key={index}>
+                <img src={item.gifLink} />
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+      );
+    }
+  }
 
   /*
    * When our component first mounts, let's check to see if we have a connected
@@ -140,11 +242,8 @@ const App = () => {
   useEffect(() => {
     if (walletAddress) {
       console.log('Fetching GIF list...');
-      
       // Call Solana program here.
-
-      // Set state
-      setGifList(TEST_GIFS);
+      getGifList()
     }
   }, [walletAddress]);
 
@@ -157,8 +256,7 @@ const App = () => {
         <div className="header-container">
           <p className="header">🚴🚴 le tour de gif</p>
           <p className="sub-text">
-            Your cycling metaverse gifhub ✨
-          </p>
+            Your cycling metaverse gifhub ✨</p>
           {!walletAddress && renderNotConnectedContainer()}
           {walletAddress && renderConnectedContainer()}
         </div>
